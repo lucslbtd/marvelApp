7735dd4993e9665d6023844bf6b10242bee65773
Lucas Martins <alucas057@gmail.com>
2022-04-27 23:50:53 -0300

    Criando nav e botton menu

---
+++ app/src/main/java/com/example/androidmarvelapp/presentation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.androidmarvelapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.androidmarvelapp.R
+import com.example.androidmarvelapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottonNavMain.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
+    }
+}
